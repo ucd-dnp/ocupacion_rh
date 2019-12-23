f25d6f9bb07eb5a356f82ba687dae907e3eaef31
david_insuasti <davidinsuasty@gmail.com>
2019-12-23 15:27:06 -0500

    Script that contain error mesages of app

---
+++ app/error_messages.py
@@ -0,0 +1,31 @@
+"""
+Diccionario que contiene los mensajes de error mostrados por las alertas
+de la aplicación de DASH
+"""
+
+
+MSJ1 = \
+"""No hay información disponible de construcciones para esta región. 
+Intente con otra región o cambie la fuente de análisis por
+'Análisis de imagen'"""
+
+MSJ2 = \
+"""No hay información disponible de capa de rios
+para esta región. Intente de nuevo o cambie
+la región de análisis"""
+
+MSJ3 = \
+"""No hay información disponible de capa de rios
+para esta región. Intente de nuevo o cambie
+la región de análisis"""
+
+MSJ4 = \
+'La región de análisis es muy grande, por favor intente con una más pequeña'
+
+MSJ5 = \
+'No se puede realizar el análisis por imágenes satelitales, por favor revise las coordenadas'
+
+MSJ6 = \
+"""No hay información disponible de capa de ríos
+para esta región. Intente de nuevo o cambie
+la región de análisis"""
